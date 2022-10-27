9ab0a6e4e90241c3cf9f7571876f2be5a3e645bb
ej8899 <ej8899@gmail.com>
2022-10-26 18:30:23 -0600

    feat: add function to buttons

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,18 @@
 import React from "react";
+import classNames from "classnames";
 
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
+
+
+  const buttonClass = classNames("button",
+  {
+    "button--confirm": props.confirm,
+    "button--danger": props.danger,
+  });
+
+  return (
+    <button disabled={props.disabled} className={buttonClass} onClick={props.onClick} >{props.children}</button>
+  );
 }
