11fdb3198d3fd0d7585ca7f0ebadb5daf0260903
ej8899 <ej8899@gmail.com>
2022-11-13 09:50:12 -0700

    feat: add additional cypress testing

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -2,4 +2,16 @@ describe("Navigation", () => {
   it("should visit root", () => {
     cy.visit("/");
   });
+
+
+  it("should navigate to Tuesday", () => {
+    cy.visit("/");
+    //cy.contains("li", "Tuesday")
+    //.click()
+    //.should("have.css", "background-color", "rgb(221, 221, 2)");
+    cy.contains("[data-testid=day]", "Tuesday")
+    .click()
+    .should("have.class", "day-list__item--selected")
+  });
+
 });
