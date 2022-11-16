7f4b9227b47636b1bcf608752188300f415b9aa7
ej8899 <ej8899@gmail.com>
2022-11-15 19:26:55 -0700

    fix: adjust application test to work with any starting day of week

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -20,10 +20,17 @@ it("defaults to Monday and changes the schedule when a new day is selected", () 
 
 it("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
   const { container, debug } = render(<Application />);
+  // stall until page is loaded
+  await waitForElement(() => getByText(container, "Monday"))
 
   // force to MONDAY for our test
-  await waitForElement(() => getByText("Monday"));
-  fireEvent.click(queryByText(container, "Monday"));
+  let day = getAllByTestId(container, "day").find(day =>
+    queryByText(day, "Monday")
+  );
+  await waitForElement(() => getByText(container,"Monday"));
+  fireEvent.click(day);
+
+
 
   await waitForElement(() => getByText(container, "Archie Cohen"));
 
@@ -43,7 +50,7 @@ it("loads data, books an interview and reduces the spots remaining for Monday by
   await waitForElement(() => queryByText(appointment, "Lydia Miller-Jones"));
   expect(getByText(appointment, "Lydia Miller-Jones")).toBeInTheDocument();
 
-  const day = getAllByTestId(container, "day").find(day => {
+  day = getAllByTestId(container, "day").find(day => {
     return queryByText(day, "Monday")
   })
 
@@ -56,6 +63,16 @@ it("loads data, books an interview and reduces the spots remaining for Monday by
 it("loads data, cancels an interview and increases the spots remaining for Monday by 1", async () => {
   //  1. Render the Application.
   const { container,debug } = render(<Application />);
+  // stall until page is loaded
+  await waitForElement(() => getByText(container, "Monday"))
+
+  // force to MONDAY for our test
+  let day = getAllByTestId(container, "day").find(day =>
+    queryByText(day, "Monday")
+  );
+  await waitForElement(() => getByText(container,"Monday"));
+  fireEvent.click(day);
+
 
   // 2. Wait until the text "Archie Cohen" is displayed.
   await waitForElement(() => getByText(container, "Archie Cohen"));
@@ -76,7 +93,7 @@ it("loads data, cancels an interview and increases the spots remaining for Monda
 
   // 7. Wait until the element with the "Add" button is displayed.
   await waitForElement(() => getByAltText(appointment, "Add"));
-    const day = getAllByTestId(container, "day").find(day =>
+  day = getAllByTestId(container, "day").find(day =>
       queryByText(day, "Monday")
     );
   // 8. Check that the DayListItem with the text "Monday" also has the text "1 spot remaining".
@@ -87,6 +104,17 @@ it("loads data, cancels an interview and increases the spots remaining for Monda
 it("loads data, edits an interview and keeps the spots remaining for Monday the same", async() => {
   //  1. Render the Application.
   const { container } = render(<Application />);
+
+  // stall until page is loaded
+  await waitForElement(() => getByText(container, "Monday"))
+
+  // force to MONDAY for our test
+  let day = getAllByTestId(container, "day").find(day =>
+    queryByText(day, "Monday")
+  );
+  await waitForElement(() => getByText(container,"Monday"));
+  fireEvent.click(day);
+
 
   // 2. Wait until the text "Archie Cohen" is displayed.
   await waitForElement(() => getByText(container, "Archie Cohen"));
@@ -103,7 +131,7 @@ it("loads data, edits an interview and keeps the spots remaining for Monday the 
 
   await waitForElement(() => getByText(container, "Sylvia Palmer"))
   expect(getByText(container, "Sylvia Palmer")).toBeInTheDocument();
-  const day = getAllByTestId(container, "day").find(day => 
+  day = getAllByTestId(container, "day").find(day => 
     queryByText(day, "Monday")
   );
   expect(getByText(day, "1 spot remaining"))
@@ -115,6 +143,18 @@ it("loads data, edits an interview and keeps the spots remaining for Monday the 
 it("shows the save error when failing to save an appointment", async() => {
   axios.put.mockRejectedValueOnce();
   const { container } = render(<Application />)
+
+  // stall until page is loaded
+  await waitForElement(() => getByText(container, "Monday"))
+
+  // force to MONDAY for our test
+  let day = getAllByTestId(container, "day").find(day =>
+    queryByText(day, "Monday")
+  );
+  await waitForElement(() => getByText(container,"Monday"));
+  fireEvent.click(day);
+
+
   await waitForElement(() => getByText(container, "Archie Cohen"))
 
   const appointment = getAllByTestId(container, "appointment").find(
@@ -141,7 +181,7 @@ it("shows the save error when failing to save an appointment", async() => {
 
   expect(getByText(container, "Archie Cohen")).toBeInTheDocument();
 
-  const day = getAllByTestId(container, "day").find(day =>
+  day = getAllByTestId(container, "day").find(day =>
       queryByText(day, "Monday")
     );
 
@@ -151,6 +191,18 @@ it("shows the save error when failing to save an appointment", async() => {
 it("shows the delete error when failing to delete an existing appointment", async () => {
   axios.delete.mockRejectedValueOnce();
   const { container } = render(<Application />);
+
+  // stall until page is loaded
+  await waitForElement(() => getByText(container, "Monday"))
+
+  // force to MONDAY for our test
+  let day = getAllByTestId(container, "day").find(day =>
+    queryByText(day, "Monday")
+  );
+  await waitForElement(() => getByText(container,"Monday"));
+  fireEvent.click(day);
+
+
   await waitForElement(() => getByText(container, "Archie Cohen"));
 
   const appointment = getAllByTestId(container, "appointment").find(
@@ -167,7 +219,7 @@ it("shows the delete error when failing to delete an existing appointment", asyn
   await waitForElement(() => getByText(appointment, "Error"));
   expect(getByText(appointment, "Error")).toBeInTheDocument();
 
-  const day = getAllByTestId(container, "day").find(day =>
+  day = getAllByTestId(container, "day").find(day =>
     queryByText(day, "Monday")
   );
 
