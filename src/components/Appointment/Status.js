9d3594754f9a92ed58990aa103cfebeeea6f86eb
ej8899 <ej8899@gmail.com>
2022-12-02 17:15:29 -0700

    ci: rebuild saving/deleting spinner to css

--- src/components/Appointment/Status.js
+++ src/components/Appointment/Status.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "components/Appointment/styles.scss";
+import "components/Appointment/spinner.scss";
 
 //
 // https://flex-web.compass.lighthouselabs.ca/workbooks/flex-m07w17/activities/900?journey_step=54
@@ -8,11 +9,12 @@ import "components/Appointment/styles.scss";
 export default function Status(props) {
   return (
     <main className="appointment__card appointment__card--status">
-      <img
-        className="appointment__status-image"
-        src="images/status.png"
-        alt="Loading"
-      />
+        <div className="spinner-box">
+        <div className="circle-border">
+          <div className="circle-core"></div>
+        </div>  
+        </div>
+
       <h1 className="text--semi-bold">{props.message}</h1>
     </main>
   );
